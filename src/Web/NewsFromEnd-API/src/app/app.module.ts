4731b640dc48b9ac1e99e2563c4748c0a0bcbfb3
Jose gonzalez <joseaugusto978@gmail.com>
2019-01-25 00:17:32 -0500

    basic from-end added

--- src/Web/NewsFromEnd-API/src/app/app.module.ts
+++ src/Web/NewsFromEnd-API/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { ErrorComponent } from './error/error.component';
+import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    ErrorComponent,
+    RegisterComponent,
+    DashboardComponent,
+    LoginComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
