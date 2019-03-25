5be869a05221b18555c6461cddd9dc309b899d64
Jose gonzalez <joseaugusto978@gmail.com>
2019-03-25 17:05:12 -0400

    UserService fully implemented fallowing REST

---
+++ src/Web/NewsFromEnd-API/src/app/helpers/jwt-interceptor.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpEvent, HttpHandler, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class JwtInterceptorService implements HttpInterceptor {
+
+  constructor(private authService: AuthService ) { }
+
+  intercept(req:  HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+     const currentUser = this.authService.currentUserValue;
+    if ( currentUser && currentUser.token) {
+        req = req.clone({
+          setHeaders: {
+            Authorization: 'Bearer ${currentUser.token}'
+          }
+        });
+    }
+    return next.handle(req);
+  }
+}
