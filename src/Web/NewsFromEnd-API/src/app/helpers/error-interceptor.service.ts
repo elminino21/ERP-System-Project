fa105e432921314521a46ace598220279a1a0af5
Jose gonzalez <joseaugusto978@gmail.com>
2019-03-25 22:49:41 -0400

    error interceptor implemented

--- src/Web/NewsFromEnd-API/src/app/helpers/error-interceptor.service.ts
+++ src/Web/NewsFromEnd-API/src/app/helpers/error-interceptor.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { AuthService } from '../services';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ErrorInterceptorService {
+export class ErrorInterceptorService implements HttpInterceptor {
 
-  constructor() { }
+  constructor( private auth: AuthService) { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+      return next.handle(req).pipe( catchError(err => {
+          if (err.status === 401) {
+            this.auth.logout();
+            location.reload(true);
+          }
+          const error = err.error.message || err.statusText;
+          return throwError(error);
+      }));
+  }
 }
