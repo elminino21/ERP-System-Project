b09c011209cff601ede07deb03c3f5cfbfb571e4
jose <Joseaugusto978@gmaiil.com>
2019-04-08 02:18:34 -0400

    project restructure & added more APIs

--- src/Services/Recieving.Api/Startup.cs
+++ src/Services/Recieving.Api/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace Newsboard.Test
+namespace Recieving.Api
 {
     public class Startup
     {
--- src/Services/Shipping.Api/Program.cs
+++ src/Services/Shipping.Api/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace Newsboard.Test
+namespace Shipping.Api
 {
     public class Program
     {
